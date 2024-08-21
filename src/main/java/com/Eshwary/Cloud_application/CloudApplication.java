25a0a7a727468420975a0d368b0ae2f745c82e5f
Naveenkumar <naveenkumar.cenimeni@achalasolutions.com>
2024-08-21 11:35:30 +0530

    cloud appliaction is pushed

---
+++ src/main/java/com/Eshwary/Cloud_application/CloudApplication.java
@@ -0,0 +1,13 @@
+package com.Eshwary.Cloud_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Eshwary/Cloud_application/CloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Eshwary.Cloud_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
